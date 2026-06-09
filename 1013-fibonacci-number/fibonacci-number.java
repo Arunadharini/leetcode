8cfae078d29378e01db30fcc490334807ae19c35
ARUNA DHARINI SEERTHI MATHIVANAN <arunadharinis@gmail.com>
2026-06-09 14:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (32.59%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int fib(int n) {
+        if(n==0||n==1 ) return n;
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return fibin(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i =2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-    public int fibin(int n ,int[] dp){
-        if(n==0 || n==1) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fibin(n-1,dp)+fibin(n-2,dp);
+        return dp[n];
+        
     }    
 }
